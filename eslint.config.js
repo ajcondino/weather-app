5e671d20636e45450d2aa1df3dddbbb6d4ae0b14
AJ Condino <a.condino@arcanys.com>
2026-06-11 15:36:01 +0800

    chore: setup husky lintstage with eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
   expoConfig,
+  // Must come after expoConfig so it overrides any conflicting formatting rules
+  eslintPluginPrettierRecommended,
   {
-    ignores: ['dist/*'],
+    ignores: ['dist/*', 'node_modules/*', '.expo/*'],
   },
 ]);
