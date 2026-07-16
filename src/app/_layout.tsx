6dcd79040698d62b4c8dbf0cdcd645fb0823eb62
AJ Condino <a.condino@arcanys.com>
2026-07-16 13:26:51 +0800

    chore: setup error boundary in root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,7 @@
+import { ErrorFallback } from '#/components/ErrorFallback';
 import { ClerkProvider } from '@clerk/expo';
 import { tokenCache } from '@clerk/expo/token-cache';
-import { Slot } from 'expo-router';
+import { ErrorBoundaryProps, Slot } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
@@ -8,6 +9,10 @@ const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 // TODO: validate env using zod or similar library
 if (!publishableKey) {
   throw new Error('Add your Clerk Publishable Key to the .env file');
+}
+
+export function ErrorBoundary({ error, retry }: ErrorBoundaryProps) {
+  return <ErrorFallback error={error} onRetry={retry} />;
 }
 
 export default function RootLayout() {
